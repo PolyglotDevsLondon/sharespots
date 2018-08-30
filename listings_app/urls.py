e087c70e30c69431c0f65e703534c3b44e7f9db1
Icicleta <icicleta@gmail.com>
2018-08-30 23:53:38 +0200

    stick to single quotes

--- listings_app/urls.py
+++ listings_app/urls.py
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage, name="homepage")
+    path('', views.homepage, name='homepage')
 ]
--- listings_app/views.py
+++ listings_app/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def homepage(request):
-    return HttpResponse("<h1>Hello! I am the homepage</h1>")
+    return HttpResponse('<h1>Hello! I am the homepage</h1>')
