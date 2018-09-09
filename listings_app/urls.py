489055d92c64cabad1116cc4f0c8e03b58997cee
Icicleta <icicleta@gmail.com>
2018-09-10 00:09:16 +0200

    create app core and move hompeage to this app

--- listings_app/urls.py
+++ listings_app/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from . import views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage, name='homepage')
+    path('core/', include('core.urls'))
 ]
