7e5c98c6f4b30f0cc9b950a515a1bf4e345bca59
Lili Kastilio <lili@lilianakastilio.co.uk>
2018-10-10 19:05:34 +0100

    fix: small fixes

--- listings_app/settings.py
+++ listings_app/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = 'listings_app.core.urls'
 
 TEMPLATES = [
     {
