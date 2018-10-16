6f31d0573591ed79137512945af611a238405c87
Dafydd Archard <dafydd_archard@me.com>
2018-10-16 13:15:36 +0100

    venue model tests

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Venue(models.Model):
+
+    name = models.CharField(max_length=150)
+    address_1 = models.CharField(max_length=150)
+    address_2 = models.CharField(max_length=150)
+    post_code = models.CharField(max_length=10)
+    description = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{0} - created at: {1}'.format(self.name, self.created_at)
