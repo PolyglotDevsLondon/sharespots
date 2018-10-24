46d44930139726693a6c737b7a66c8aed3760779
Tsvetelina Koleva <tsekoleva@gmail.com>
2018-10-25 00:25:58 +0100

    latest alt to templates folder

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def homepage(request):
-    return HttpResponse('<h1>Hello! I am the homepage</h1>')
+    return render(request, 'core/index.html')
