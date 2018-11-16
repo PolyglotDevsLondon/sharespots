8dbcfa763805482315cdf4758ffc2b0c4e3bb918
Dafydd Archard <dafydd_archard@me.com>
2018-11-16 20:56:00 +0000

    opened up spaces in view comments

--- core/views.py
+++ core/views.py
@@ -7,12 +7,13 @@ def homepage(request):
 
 def cafe(request, id):
     venue = VenueHelpers()
+    
     ## UNCOMMENT THE BELOW TO SETUP DEFAULT VENUE FOR DEVELOPMENT ##
+
     ##if not venue.check_default_venue_exists():
        ## venue.create_default_venue()
     ##default_venue = venue.get_default_venue()
 
     ## GET A CAFE BY IT'S ID ##
     cafe = venue.get_venue(id)
-    
     return render(request, 'core/cafe.html', {'cafe':cafe})
