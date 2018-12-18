50e8e3e1ee8104ecd18f864a864c706840f59641
Ju-Vern See <juvern.see@gmail.com>
2018-12-18 20:15:34 +0000

    Change description field type to TextField

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ class Venue(models.Model):
     address_1 = models.CharField(max_length=150)
     address_2 = models.CharField(max_length=150)
     post_code = models.CharField(max_length=10)
-    description = models.CharField(max_length=1000)
+    description = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
