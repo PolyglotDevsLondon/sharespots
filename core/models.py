ad5f1c1123b9a583957ec09efa405600931d92f6
Icicleta <icicleta@gmail.com>
2018-12-28 00:09:53 +0100

    create model rating, add it to admin

--- core/models.py
+++ core/models.py
@@ -1,4 +1,11 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+
+class Rating(models.Model):
+
+    rate = models.IntegerField(default=0, validators=[MaxValueValidator(5), MinValueValidator(0)])
+
 
 class Venue(models.Model):
 
@@ -8,6 +15,7 @@ class Venue(models.Model):
     post_code = models.CharField(max_length=10)
     description = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.ForeignKey(Rating, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return '{0} - created at: {1}'.format(self.name, self.created_at)
