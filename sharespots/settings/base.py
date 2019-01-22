cba1a558d4c4689a92f3b5fe066ac7ad2fdcf951
Liliana Kastilio <lili@snyk.io>
2019-01-22 22:30:06 +0000

    fix: Fix template and static path

--- sharespots/settings/base.py
+++ sharespots/settings/base.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sharespots.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, '..', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend/dist'),
+    os.path.join(BASE_DIR, '..', 'frontend/dist'),
 )
 
 # Activate Django-Heroku.
