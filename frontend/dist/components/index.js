cdc9637a2ad87df376d18c7aff84e0500ebb05e2
Leo <leo.kwok@beamly.com>
2019-01-23 20:28:26 +0000

    amend webpack and readme file

--- frontend/dist/components/index.js
+++
@@ -1,7 +0,0 @@
-import hamburgerComponent from './hamburger.js';
-
-export default function initComponents() {
-    console.log('****** initComponents() ******');
-    hamburgerComponent();
-}
-//# sourceMappingURL=index.js.map
