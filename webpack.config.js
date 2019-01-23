f7db47887165e3ecf8f64a581042adc2903be7bf
Leo <leo.kwok@beamly.com>
2019-01-23 18:55:47 +0000

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,27 @@
 const path = require('path');
 
 module.exports = {
-    entry: './frontend/src/scripts/index.js',
+    entry: './frontend/src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'main.js',
         path: path.resolve(__dirname, './frontend/dist')
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+          },
+          {
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        }]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
     }
 };
