69d406b548357d0ca0ead8aa7df48e24aa5273eb
Lili Kastilio <lili@lilianakastilio.co.uk>
2019-02-17 23:57:35 +0000

    fix: rename DJANGO_SECRET_KEY to SECRET_KEY

--- sharespots/settings/staging.py
+++ sharespots/settings/staging.py
@@ -1,4 +1,3 @@
 from .base import *
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h)wmbja@&kvo3#0yyv@piqcp6*(xs=r5!2grbiej8y2!issrn@'
+SECRET_KEY = os.environ['SECRET_KEY']
