b46f3036e8636fa3c62467c6e8a079f54d20c892
Ravi <ravinder9.reddy@gmail.com>
2020-05-24 07:37:49 +0100

    basic FE setup to veriy BE

--- search/views.py
+++ search/views.py
@@ -6,4 +6,5 @@ from django.http import HttpResponse
 def search(request):
     search_venue = request.GET.get('search')
     searched_venues = Venue.objects.filter(name__icontains=search_venue)
-    return render(request, 'search/venue_list.html', {'searched_venues': searched_venues})
+    context = {'searched_venues': searched_venues}
+    return render(request, 'search/venue_list.html', context)
