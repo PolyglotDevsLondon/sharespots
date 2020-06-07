792ca48a2a14a3eaf6dada62ee7f04ae66c9665f
Lili Kastilio <lili@lilianakastilio.co.uk>
2020-06-07 12:36:37 +0100

    fix: force set allowed hosts

--- sharespots/settings/base.py
+++ sharespots/settings/base.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-DEBUG = True
+DEBUG = get_env('DJANGO_DEBUG', False)
 
 
 ############
