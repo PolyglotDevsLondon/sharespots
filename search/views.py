bcb30db4c425b91368c48aee911a2a7896b67119
Ravi <ravinder9.reddy@gmail.com>
2020-06-12 21:24:36 +0100

    Updated FTS - SearchRak filter

--- search/views.py
+++ search/views.py
@@ -11,6 +11,6 @@ def search(request):
     vector = SearchVector('name', weight='A', config='english') + SearchVector('post_code', weight='B', config='english')
     query = SearchQuery(user_search_venue)
     rank = SearchRank(vector, query)
-    searched_venues = Venue.objects.annotate(rank=rank).filter(rank__gte=0.3).order_by('-rank')
+    searched_venues = Venue.objects.annotate(rank=rank).filter(rank__gte=0.2).order_by('-rank')
     context = {'searched_venues': searched_venues}
     return render(request, 'search/venues.html', context)
