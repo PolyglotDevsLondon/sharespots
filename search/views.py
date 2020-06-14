b1cf827b2ca8ee275b4bfdb9b0f411d4c849d2d6
Ravi <ravinder9.reddy@gmail.com>
2020-06-14 16:59:54 +0100

    Enabled FTS for Venue names & Postcode

--- search/views.py
+++ search/views.py
@@ -1,16 +1,15 @@
 from core.models import Venue
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from django.db.models import Value, TextField
-from django.db.models.functions import Concat
+from django.db.models.functions import Concat, Greatest
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 
 def search(request):
     user_search_venue = request.GET.get('search')
-    vector = SearchVector('name', weight='A', config='english') + SearchVector('post_code', weight='B', config='english')
-    query = SearchQuery(user_search_venue)
-    rank = SearchRank(vector, query)
-    searched_venues = Venue.objects.annotate(rank=rank).filter(rank__gte=0.2).order_by('-rank')
-    context = {'searched_venues': searched_venues}
+    trigram_check = Venue.objects.annotate(
+        similarity=Greatest(TrigramSimilarity('post_code', user_search_venue), TrigramSimilarity('name', user_search_venue)
+    )).filter(similarity__gte=0.3).order_by('-similarity')
+    context = {'searched_venues': trigram_check}
     return render(request, 'search/venues.html', context)
