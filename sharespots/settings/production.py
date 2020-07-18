8de474479544bdd716f5ce9c67523ba671ac627e
abuDarda97 <abudarda166@gmail.com>
2020-07-18 15:24:44 +0100

    Moved django heroku settings to staging and production

--- sharespots/settings/production.py
+++ sharespots/settings/production.py
@@ -2,3 +2,6 @@ from .base import *
 
 # access heroku environment environment-variable (config variable) of the secret key
 SECRET_KEY = get_env('SECRET_KEY')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals(), logging=True, allowed_hosts=False, databases=not DEBUG)
