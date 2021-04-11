30c66bbecc334d01c74d71dc40644ca571da343e
Abu <abudarda166@gmail.com>
2021-04-11 16:36:41 +0100

    Add AWS bucket
    * settings file configured correctly

--- sharespots/settings/base.py
+++ sharespots/settings/base.py
@@ -144,6 +144,11 @@ NOTEBOOK_ARGUMENTS = [
 # Storages S3 #
 ###############
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 AWS_ACCESS_KEY_ID = get_env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = get_env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = get_env('AWS_STORAGE_BUCKET_NAME', 'sharespots-local')
+AWS_STORAGE_BUCKET_NAME = get_env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
